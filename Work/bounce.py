196f1d15be90c9548d3b75f9a0678cc2eb5f8436
Ezra Tucker <ezra.tucker@energeticinsurance.com>
2023-07-25 16:54:05 -0400

    bounce and mortgage

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,12 @@
 # bounce.py
 #
 # Exercise 1.5
+
+i = 0
+height = 60
+while i < 10:
+    print(round(height, 4))
+    height *= 3/5
+    i += 1
+
+
--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,17 @@
 # mortgage.py
 #
 # Exercise 1.7
+
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+period = 0
+
+while principal > 0:
+    extra = 1000 if period < 12 else 0
+    principal = principal * (1 + rate / 12) - payment - extra
+    total_paid = total_paid + payment + extra
+    period += 1
+
+print('Total paid', total_paid)
